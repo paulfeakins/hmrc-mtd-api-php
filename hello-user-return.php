df2815b951ae53f1f2c2c4351b9a3c8302aa4257
paulfeakins <paulfeakins@gmail.com>
2018-11-12 17:38:49 +0000

    Update hello-user-return.php

--- hello-user-return.php
+++ hello-user-return.php
@@ -19,33 +19,33 @@
 	// POST the data to HMRC to get an access token.
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, 'https://test-api.service.hmrc.gov.uk/oauth/token');
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_field_string);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $post_field_string);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 	$response = curl_exec($ch);
 	curl_close ($ch);
 	
 	// Convert from JSON to PHP.
 	$response = json_decode($response, true);
 
-    // Check to see if we have an access token.
-    if(!isset($response['access_token'])) {
-        // We couldn't get an access token.
-        echo 'No access token.';
-    } else {
-        // Try to call the API.
-        $access_token = $response['access_token'];
-        $headers = [
-            'Accept: application/vnd.hmrc.1.0+json',
-            'Authorization: Bearer ' . $access_token
-        ];
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://test-api.service.hmrc.gov.uk/hello/user");
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        $server_output = curl_exec ($ch);
-        curl_close ($ch);
+	// Check to see if we have an access token.
+	if(!isset($response['access_token'])) {
+		// We couldn't get an access token.
+		echo 'No access token.';
+	} else {
+		// Try to call the API.
+		$access_token = $response['access_token'];
+		$headers = [
+		    'Accept: application/vnd.hmrc.1.0+json',
+		    'Authorization: Bearer ' . $access_token
+		];
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, "https://test-api.service.hmrc.gov.uk/hello/user");
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+		$server_output = curl_exec ($ch);
+		curl_close ($ch);
 
-        // Print what the hello user API returns.
-        echo $server_output;
-    }
+		// Print what the hello user API returns.
+		echo $server_output;
+	}
 	
